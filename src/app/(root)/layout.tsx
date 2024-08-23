aebe475b853644fac164393d3d283900abaa1bc1
Robin <mahamuniaditya07@gmail.com>
2024-08-24 00:55:33 +0530

    adding side bar and topbar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,12 +2,15 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "../globals.css";
+import { Providers } from "./providers"
+import { Sidebar } from "@/components/shared/Sidebar";
+import { TopBar } from "@/components/shared/TopBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Auth",
-  description: "Quick auth",
+  title: "Quick",
+  description: "Quick",
 };
 
 export default function RootLayout({
@@ -16,10 +19,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-      <html lang='en'>
-        <body className={`${inter.className} bg-dark-1 flex`}>
-          {children}
-          </body>
-      </html>
+    <html lang='en'>
+      <body className={`${inter.className} bg-dark-1 flex`}>
+        <Providers >
+          <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+            <Sidebar />
+            <div className="flex flex-col">
+              <TopBar />
+              {children}
+            </div>
+          </div>
+        </Providers>
+      </body>
+    </html>
   );
 }
